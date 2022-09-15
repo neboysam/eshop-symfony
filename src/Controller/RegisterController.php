cc5618500fe3d5fdad64e73645dd70bc038aeabd
neboysam <monte.nemi@gmail.com>
2022-09-15 15:26:56 +0200

    created project, registration and login

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager) {
+        return $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/inscription", name="app_register")
+     */
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $notification = '';
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($form->get('confirm_password')->getData());
+            /* $password = $form->get('password')->getData();
+            $confirm_password = $form->get('confirm_password')->getData();
+
+            if ($password === $confirm_password) {
+                $hashed_password = $passwordHasher->hashPassword($user, $password);
+                $user->setPassword($hashed_password);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                $notification = "L'inscription a ete effectue avec success !";
+            } else {
+                $notification = 'Le mot de passe et le de mot de passe confirme doivent etre identiques.';
+            } */
+
+            $db_user_array = $this->entityManager->getRepository(User::class)->findByEmail($form->get('email')->getData());
+
+            if(!empty($db_user_array)) {
+                $notification = "L'utilisateur existe deja.";
+                } else {
+                    $password = $form->get('password')->getData();
+                    $hashed_password = $passwordHasher->hashPassword($user, $password);
+                    $user->setPassword($hashed_password);
+                    $this->entityManager->persist($user);
+                    $this->entityManager->flush();
+                    $notification = "L'inscription a ete effectue avec success !";
+                }
+            }
+
+        return $this->render('register/register.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prenom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prenom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email'
+                ]
+            ])
+            /* ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe'
+                ]
+            ])
+            ->add('confirm_password', PasswordType::class, [
+                'label' => 'Confirmer votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                ]
+            ]) */
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent etre identiques.',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe', 'attr' => ['placeholder' => 'Merci de saisir votre mot de passe']],
+                'second_options' => ['label' => 'Confirmez votre mot de passe', 'attr' => ['placeholder' => 'Merci de confirmer votre mot de passe']]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+                'attr' => [
+                    'class' => 'btn btn-info btn-block'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
